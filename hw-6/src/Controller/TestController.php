cee825f3b869a63810da303e8e9f1fdad2a37b89
qoterwater <marderics@gmail.com>
2021-05-06 19:12:20 +0300

    m

---
+++ hw-6/src/Controller/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * @Rote("/test") 
+ */
+
+class TestController
+{
+    /**
+     * @Route(path="/')
+     */
+        
+    public function index() {
+
+    }
+}
