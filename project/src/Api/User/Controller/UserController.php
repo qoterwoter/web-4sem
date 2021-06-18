c00081dd6782b6db86068807f2b682577df56655
qoterwater <marderics@gmail.com>
2021-06-18 23:57:27 +0300

    papa

--- project/src/Api/User/Controller/UserController.php
+++ project/src/Api/User/Controller/UserController.php
@@ -28,7 +28,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 /**
- * @Route("/users")
+ * @Route("/students")
  */
 class UserController extends AbstractController
 {
